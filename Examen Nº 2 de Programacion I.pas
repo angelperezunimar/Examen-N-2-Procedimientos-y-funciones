2098dad2891478d4b5ed0b0631c59022e8e052d0
angelperezunimar <aperez.9625@unimar.edu.ve>
2023-03-15 15:05:10 -0400

    Se plantea las necesidades del programa en forma de comentario

--- Examen Nº 2 de Programacion I.pas
+++ Examen Nº 2 de Programacion I.pas
@@ -1,3 +1,13 @@
+// Crear un programa que permita imprimir un número ingresado por el usuario en un
+// color específico si cumple con alguno de los siguientes parámetros:
+// 1. Es divisible entre 13, de serlo deberá imprimirse el número en color marrón.
+// 2. Es divisible entre 7, de serlo deberá imprimirse el número en color verde.
+// 3. Es divisible entre 5, de serlo deberá imprimirse el número en color rojo.
+// 4. Es divisible entre 3, de serlo deberá imprimirse el número en color azul.
+// 5. Es divisible entre 2, de serlo deberá imprimirse el número en color amarillo.
+// 6. De no ser divisible en base de cualquiera de los casos expuestos deberá imprimir el
+// texto en color blanco.
+
 program untitled;
 
 uses crt;
