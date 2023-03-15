0502ec5911414cf70b077bb279af0c077bf87406
angelperezunimar <aperez.9625@unimar.edu.ve>
2023-03-15 16:31:54 -0400

    Se plantean las necesidades del programa

--- Examen Nº 2 de Programacion I.pas
+++ Examen Nº 2 de Programacion I.pas
@@ -1,3 +1,12 @@
+//Desarrolle un programa en Pascal que permita al usuario ingresar una frase y a su
+//vez tenga las opciones de convertir esa frase ingresada en números Binarios,
+//Hexadecimal, Octal y Romano. Después de convertir la frase a cualquiera de las
+//opciones propuestas, el programa debe tener una opción para seguir haciendo
+//conversiones. Tener en cuenta que el programa también debe mostrar la opción de salir
+//o de convertir otra frase.
+
+
+
 program untitled;
 
 uses crt;
